b5f6d8138afe5543143a17db7950b90319f6ff34
kimsangri <ksl660@naver.com>
2023-12-30 01:54:14 +0900

    publish

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,7 +5,7 @@ function Movie({id,coverImg,title,summary,genres}){
     return (
         <div>
           <img src={coverImg} alt={title}/>
-          <h2><Link to={`/movie/${id}`}>{title}</Link></h2>
+          <h2><Link to={`${process.env.PUBLIC_URL}/movie/${id}`}>{title}</Link></h2>
           <p>{summary}</p>
           <ul>
             <li>{genres.map(g => <li key={g}>{g}</li>)}</li>
