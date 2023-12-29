4ae7428989274060b641e63d222c555d2e1c4ccf
kimsangri <ksl660@naver.com>
2023-12-30 02:33:42 +0900

    add slice

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -6,7 +6,7 @@ function Movie({id,coverImg,title,summary,genres}){
         <div>
           <img src={coverImg} alt={title}/>
           <h2><Link to={`${process.env.PUBLIC_URL}/movie/${id}`}>{title}</Link></h2>
-          <p>{summary}</p>
+          <p>{summary.length > 235 ? `${summary.slice(0,235)}...` : summary}</p>
           <ul>
             <li>{genres.map(g => <li key={g}>{g}</li>)}</li>
           </ul>
